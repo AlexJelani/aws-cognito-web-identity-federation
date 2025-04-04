9b655602fe8eca819f248edd16ff42d9f7cc4f97
AlexJelani <jelanialexander82@gmail.com>
2025-04-04 12:52:48 +0900

    Initial commit

---
+++ test/aws-cognito-web-identity-federation.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCognitoWebIdentityFederation from '../lib/aws-cognito-web-identity-federation-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cognito-web-identity-federation-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCognitoWebIdentityFederation.AwsCognitoWebIdentityFederationStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
